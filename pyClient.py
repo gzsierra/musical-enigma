3a12082409faccbbdb29a0496feebc340c8c50d9
Nicolas Nadeau <niconadeau0@gmail.com>
2016-07-19 15:13:05 -0400

    Final script

--- pyClient.py
+++ pyClient.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
 import psutil, sendInfo
 
 while 1==1 :
-    cpu = psutil.cpu_percent(interval=5)
+    # Every second, it will fetch data
+    cpu = psutil.cpu_percent(interval=1)
     mem = psutil.virtual_memory().percent
 
-    # print("CPU : " + str(cpu) + "   Mem : " + str(mem), end='\r')
+    sendInfo.update(str(cpu), str(mem))
 
-    sendInfo.sendInfo(cpu, mem)
+    # For debug
+    # print("CPU : " + str(cpu) + "   Mem : " + str(mem), end='\r')
--- sendInfo.py
+++ sendInfo.py
@@ -1,31 +1,20 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, rc):
-    print("Connected with result code "+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
+# Send to the server
 def send(subscriptions, value):
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-
-    # Host port keepalive bin_addr
-    # client.connect("172.24.20.26", 1883, 60)
-    client.connect("192.168.1.72", 1883, 60)
-
+    client.publish(subscriptions, value)
     # To test if work, on server use command :
     # mosquitto_sub -d -t paho/temperature
-    client.publish(subscriptions, value)
-    print("Msg sent")
 
-def sendInfo(cpu, ram):
+# receives what to send
+def update(cpu, ram):
     send("client/CPU", cpu)
     send("client/RAM", ram)
+
+################### PRELOAD ###################
+client = mqtt.Client()
+
+# Host port keepalive bin_addr
+client.connect("172.24.20.26", 1883, 60)
+# client.connect("192.168.1.72", 1883, 60)
